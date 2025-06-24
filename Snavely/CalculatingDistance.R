4d7f1dcd4c92b3bc7f713f8f6926a22921d4746e
Luke Snavely <lsnavely28@gmail.com>
2025-06-24 15:18:16 -0400

    Loading in data

--- Snavely/CalculatingDistance.R
+++ Snavely/CalculatingDistance.R
@@ -1,25 +1,22 @@
-# Test
 library(tidyverse)
-tracking <- read_csv("data/tracking_week_1.csv") |> 
-  bind_rows(read_csv("data/tracking_week_2.csv")) |> 
-  bind_rows(read_csv("data/tracking_week_3.csv"))
 
+# tracking <- read_csv("data/tracking_week_1.csv") |>
+#   bind_rows(read_csv("data/tracking_week_2.csv")) |>
+#   bind_rows(read_csv("data/tracking_week_3.csv")) |>
+#   bind_rows(read_csv("data/tracking_week_4.csv")) |>
+#   bind_rows(read_csv("data/tracking_week_5.csv")) |>
+#   bind_rows(read_csv("data/tracking_week_6.csv")) |>
+#   bind_rows(read_csv("data/tracking_week_7.csv")) |>
+#   bind_rows(read_csv("data/tracking_week_8.csv")) |>
+#   bind_rows(read_csv("data/tracking_week_9.csv"))
 
-# library(tidyverse)
-tracking <- read_csv("data/tracking_week_1.csv") |>
-  bind_rows(read_csv("data/tracking_week_2.csv")) |>
-  bind_rows(read_csv("data/tracking_week_3.csv")) |>
-  bind_rows(read_csv("data/tracking_week_4.csv")) |>
-  bind_rows(read_csv("data/tracking_week_5.csv")) |>
-  bind_rows(read_csv("data/tracking_week_6.csv")) |>
-  bind_rows(read_csv("data/tracking_week_7.csv")) |>
-  bind_rows(read_csv("data/tracking_week_8.csv")) |>
-  bind_rows(read_csv("data/tracking_week_9.csv"))
+# # write the combined tracking data object into a parquet file
+# library(arrow)
+# arrow::write_parquet(tracking, "data/tracking.parquet")
 
+tracking <- arrow::read_parquet("data/tracking.parquet")
 
+games <- read_csv("data/games.csv")
 
-# write the combined tracking data object into a parquet file
-# install.packages("arrow")
-library(arrow)
-arrow::write_parquet(tracking, "data/tracking.parquet")
-
+players <- read_csv("data/players.csv")
+head(tracking, 10)
