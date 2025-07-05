c0dd14ad971393ebae9337001230cad57b98912f
Sheryl Solorzano <ssolorzano27@amherst.edu>
2025-07-05 17:05:15 -0400

    scatterplot matrix of clusters based on scaled variables

---
+++ Sheryl/kmeans_clustering.R
@@ -0,0 +1,72 @@
+# loading packages
+
+library(tidyverse)
+library(ggrepel)
+library(GGally)
+
+
+# removing specific dataset from environment
+rm(rb_plays_clust)
+
+# clustering
+## standardizing variables======================================================
+rb_plays_clust <- rb_stats_per_play |>
+  filter(displayName == "Josh Jacobs") |>
+  select(displayName, mean_ke, dis_gained_x, avg_jerk, expectedPointsAdded) |>
+  mutate(across(where(is.numeric),
+                ~ (.x - mean(.x)) / sd(.x),
+                .names = "{.col}_sd")) |>
+    slice_sample(n = 50) |>
+  select(ends_with("_sd"))
+
+#========================== elbow plot=========================================
+
+set.seed(231)
+
+elbow_plot <- tibble(k = 1:10) |>
+  mutate(
+    kmeans_results = purrr::map(k, ~kmeans(rb_plays_clust, .x, nstart = 30)),
+    glanced = purrr::map(kmeans_results, glance)) |>
+  unnest(cols = c(glanced))
+
+
+# making the elbow plot
+ggplot(elbow_plot, aes(x = k, y = tot.withinss)) +
+  geom_point() +
+  geom_line() +
+  scale_x_continuous(breaks = 1:10) +
+  labs(x = "Num of clusters(k)",
+       y = "Total within-cluster sum of squares")
+
+
+## Clustering standardized variables=============================================
+
+rb_plays_clust_scaled <- rb_plays_clust |>
+  kmeans(centers = 6, nstart = 30) # decide number of clusters with elbow plot
+
+# cluster assignments using the standardized data frame and filtered & sampled subset=
+# on Josh Jacobs=================================================================== 
+
+rb_plays_clust_assig <- augment(rb_plays_clust_scaled, rb_plays_clust) |>
+  rename(cluster_standardized = .cluster)
+
+
+# scatterplot matrix of clusters based on scaled variables ======================
+
+rb_plays_clust_assig |>
+  select(mean_ke_sd, dis_gained_x_sd, avg_jerk_sd, expectedPointsAdded_sd, 
+         cluster_standardized) |>
+  ggpairs(aes(color = cluster_standardized),
+          upper = list(continuous = "blank"),
+          progress = FALSE) +
+  theme(
+    # change all text to black, including strip labels and axis titles
+    text = element_text(color = "black", size = 11),
+    strip.text = element_text(color = "black"),  # variable names on top of panels
+    axis.text = element_text(color = "black")    # axis tick labels
+  )
+
+
+
+
+
