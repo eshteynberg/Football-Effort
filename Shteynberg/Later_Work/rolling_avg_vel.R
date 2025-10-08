9d0f4bea1403544f7fca38f969836a55ff06ee34
Luke Snavely <lsnavely28@gmail.com>
2025-10-08 14:44:14 -0400

    Updated graphs for rolling average

--- Shteynberg/Later_Work/rolling_avg_vel.R
+++ Shteynberg/Later_Work/rolling_avg_vel.R
@@ -309,3 +309,88 @@ qgam_dis_player <- qgam_dis |>
   mutate(dis_score = round(dis_score, 4) *100,
          rank = 1:n())
 
+# Graph -------------------------------------------------------------------
+qgam_graph <- function(name) {
+  qgam_dis2 <- qgam_dis |> 
+    filter(displayName == name)
+  
+  qgam_pos <- qgam_dis |> 
+    filter(displayName == name,
+           qgam_pred >= 0)
+  
+  qgam_neg <- qgam_dis |> 
+    filter(displayName == name,
+           qgam_pred < 0)
+  
+  graph <- qgam_dis2 |> 
+    ggplot(aes(x = s_5, y = dir_a_mpsh_5)) +
+    geom_point(alpha=.6, aes(fill = dis_score_adj), pch = 21, size = 3) +
+    geom_line(data = qgam_pos, aes(y = qgam_pred, color = "0.95 quantile accel. \nregression line"), lwd = 1.3) +
+    geom_line(data = qgam_neg, aes(y = qgam_pred, color = "0.95 quantile decel. \nregression line"), lwd = 1.3) +
+    geom_hline(aes(yintercept = 0), color = "black", lwd = 1.3, lty = 2) +
+    scale_color_manual("Line", values = c("#D50A0A", "#0072B2")) +
+    scale_fill_gradient(name = "Effort Score", low = "#88CCEE", high = "#CC6677") +
+    labs(x = "Speed (mph)",
+         y = "Acceleration (mph/s)",
+         title = paste0(name)) +
+    theme_minimal(base_size=16) +
+    theme(plot.title = element_text(face = "bold.italic",
+                                    size = 18, 
+                                    hjust = .5),
+          legend.title = element_text(face = "bold"),
+          axis.title = element_text(face = "bold"),
+          legend.text=element_text(size=15),
+          plot.caption = element_text(face = "italic", size = 8),
+          legend.key.height = unit(1.4, "cm")) +
+    guides(
+      fill = guide_legend(order = 1),
+      color = guide_legend(order = 2)
+    ) +
+    xlim(0, 25) +
+    ylim(-20, 20)
+  
+  return(graph)
+}
+
+qgam_graph("Derrick Henry")
+
+
+
+# GT Table ----------------------------------------------------------------
+
+library(gt)
+library(gtExtras)
+library(nflplotR)
+library(nflreadr)
+
+# Getting player heads
+rosters <- nflreadr::load_rosters(2022) |> 
+  mutate(gsis_it_id = as.numeric(gsis_it_id))
+
+qgam_dis_player |>
+  slice_max(dis_score, n = 10) |> 
+  left_join(select(rosters, bc_id = gsis_it_id, gsis_id)) |> 
+  select(displayName, gsis_id, dis_score, rank) |> 
+  gt() |>
+  tab_header(title = md("**Top RBs for Effort Score**"),
+             subtitle = md("*Utilized QGAM to calculate score*")) |>
+  cols_label(displayName = "Player", gsis_id = "", dis_score = "Effort Score %", rank = "Rank") |>
+  nflplotR::gt_nfl_headshots(columns = gsis_id, height = 60) |> 
+  data_color(columns = c(dis_score),
+             fn = scales::col_numeric(palette = c("white", "#D50A0A"), domain = NULL))  |>
+  gtExtras::gt_theme_espn() |>
+  opt_align_table_header(align = "center")
+
+qgam_dis_player |>
+  slice_min(dis_score, n = 10) |> 
+  left_join(select(rosters, bc_id = gsis_it_id, gsis_id)) |> 
+  select(displayName, gsis_id, dis_score, rank) |> 
+  gt() |>
+  tab_header(title = md("**Bottom RBs for Effort Score**"),
+             subtitle = md("*Utilized QGAM to calculate score*")) |>
+  cols_label(displayName = "Player", gsis_id = "", dis_score = "Effort Score %", rank = "Rank") |>
+  nflplotR::gt_nfl_headshots(columns = gsis_id, height = 60) |> 
+  data_color(columns = c(dis_score),
+             fn = scales::col_numeric(palette = c("#0072B2", "white"), domain = NULL))  |>
+  gtExtras::gt_theme_espn() |>
+  opt_align_table_header(align = "center")
